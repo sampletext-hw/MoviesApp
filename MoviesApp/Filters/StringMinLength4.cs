735bdf7044978475ef0bec653088d8c8da93372a
Bird Egop <sampletext32@bk.ru>
2020-12-08 12:16:36 +0300

    Add custom StringMinLength4 Filter

---
+++ MoviesApp/Filters/StringMinLength4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MoviesApp.ViewModels;
+
+namespace MoviesApp.Filters
+{
+    public class StringMinLength4 : ValidationAttribute
+    {
+        public StringMinLength4()
+        {
+        }
+
+        public string GetErrorMessage() =>
+            $"Actor must have at least 4 chars name and surname";
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var str = (string)value;
+            if (str.Length < 4)
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- MoviesApp/ViewModels/InputActorViewModel.cs
+++ MoviesApp/ViewModels/InputActorViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using MoviesApp.Filters;
 
 namespace MoviesApp.ViewModels
 {
     public class InputActorViewModel
     {
+        [StringMinLength4]
         public string Name { get; set; }
 
+        [StringMinLength4]
         public string Surname { get; set; }
 
         [DataType(DataType.Date)]
