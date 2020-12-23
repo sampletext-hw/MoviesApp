393d14bbff7d8cc9ce7a1575e2d965229ee15f24
Bird Egop <sampletext32@bk.ru>
2020-12-23 19:38:51 +0300

    Extend logging middleware

--- MoviesApp/Middlewares/ActorControllerLoggerMiddleware.cs
+++ MoviesApp/Middlewares/ActorControllerLoggerMiddleware.cs
@@ -20,9 +20,23 @@ namespace MoviesApp.Middlewares
 
         public async Task InvokeAsync(HttpContext httpContext)
         {
-            if (httpContext.Request.Path.Value.ToLower().Contains("api/actor"))
+            if (httpContext.Request.Path.Value.ToLower().Contains("actor"))
             {
-                _logger.LogDebug($"Actor Request: {httpContext.Request.Path.Value}, Method={httpContext.Request.Method}");
+                _logger.LogDebug(
+                    $"ContentType: {httpContext.Request.ContentType}\n" +
+                    $"ContentLength: {httpContext.Request.ContentLength}\n" +
+                    $"Query: {string.Join(", ", from q in httpContext.Request.Query select $"{q.Key} = {string.Join(",", q.Value)};")}\n" +
+                    $"Cookies: {string.Join(", ", from c in httpContext.Request.Cookies select $"{c.Key} = {c.Value};")}\n" +
+                    $"HasFormContentType: {httpContext.Request.HasFormContentType}\n" +
+                    $"Headers: {string.Join(", ", from h in httpContext.Request.Headers select $"{h.Key} = {string.Join(",", h.Value)};")}\n" +
+                    $"Host: {httpContext.Request.Host.Value}\n" +
+                    $"IsHttps: {httpContext.Request.IsHttps}\n" +
+                    $"Method: {httpContext.Request.Method}\n" +
+                    $"PathBase: {httpContext.Request.PathBase.Value}\n" +
+                    $"Protocol: {httpContext.Request.Protocol}\n" +
+                    $"QueryString: {httpContext.Request.QueryString.Value}\n" +
+                    $"RouteValues: {string.Join(", ", from rv in httpContext.Request.RouteValues select $"{rv.Key}={rv.Value};")}\n" +
+                    $"Scheme: {httpContext.Request.Scheme}");
             }
 
             await _next.Invoke(httpContext);
